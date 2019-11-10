b3b146ce5f38291713567d7eebd3f0540177ac92
Matheus A. Lino <matheus_araujolino@hotmail.com>
2019-11-10 19:38:31 -0300

    [add] DAO files created (not implemented)

---
+++ Application/InventoryApp/DAO/CategoryDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp.DAO
+{
+    class CategoryDAO
+    {
+        // Attributes
+        private Connection conn = new Connection();
+    }
+}
---
+++ Application/InventoryApp/DAO/ProductDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp.DAO
+{
+    class ProductDAO
+    {
+        // Attributes
+        private Connection conn = new Connection();
+    }
+}
